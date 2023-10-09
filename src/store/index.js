5bdc795ac060b582d8b951db9f359afb9b806474
mr-mukhtar <mukhtarmohd517@gmail.com>
2023-10-09 14:44:04 +0530

    ubscribing and Dispatching Actions

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,12 @@ import { createStore } from 'redux';
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'incriment') {
     return {
-      counter: state.counter + 1,
+      counter: state.counter + 5,
     };
   }
   if (action.type === 'decriment') {
     return {
-      counter: state.counter - 1,
+      counter: state.counter - 5,
     };
   }
   return state;
